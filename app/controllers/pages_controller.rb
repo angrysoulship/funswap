2cdcb1b4a08d93013fbd2690f8221a276bc112a0
ivyf <iveee1023@gmail.com>
2021-07-19 18:08:20 +0800

    tailwindcss install

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def home
