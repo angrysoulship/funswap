a094baedb724dc0695a1f576f1151ece615e75cd
ivyf <iveee1023@gmail.com>
2021-07-19 17:11:04 +0800

    default-setting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def home
+  end
+
+
+
+  private 
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
