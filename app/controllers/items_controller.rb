037ad6220060e40a7cd37513e3d80d658eaf7506
eo hao <hash1hao@gmail.com>
2021-07-20 21:15:14 +0800

    conflict fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,15 +11,12 @@ class ItemsController < ApplicationController
     @collection = Collection.find(params[:collection_id])
     @item.collection = @collection
     @item.save
-<<<<<<< HEAD
-=======
     authorize @item
     redirect_to collection_path(@collection)
   end
 
   def show
     @item = Item.find(params[:id])
->>>>>>> ccb7c7ef85502547d6c39a22b189fb134c900b12
     authorize @item
   end
 
