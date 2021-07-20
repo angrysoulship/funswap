2709239adaeacb3582151c93f1df9e8b33ac622e
ivyf <iveee1023@gmail.com>
2021-07-20 19:29:53 +0800

    item_mvc_v1

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,34 @@
+class ItemsController < ApplicationController
+
+  def new
+    @collection = Collection.find(params[:collection_id])
+    @item = Item.new
+    authorize @item
+  end
+
+  def create
+    @item = Item.new(collection_params)
+    @collection = Collection.find(params[:collection_id])
+    @item.collection = @collection
+    @item.save
+    raise
+    authorize @item
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def collection_params
+    params.require(:item).permit(:name, :species, :img_url, :description, :price, :collection_id, :favorite)
+  end
+
+
+end
