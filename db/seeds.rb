88ed14298785a3ce5b026a984b72a8897eae17f8
ivyf <iveee1023@gmail.com>
2021-07-21 16:20:47 +0800

    seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,43 +13,43 @@ Item.destroy_all
 # Collection.create(name: "Young Pokemons!", user_id: 1)
 # Collection.create(name: "Grown-ups!", user_id: 1)
 
-# Item.create!(name: "Psyduck", species: "Water", description: "Psyduck is constantly beset by headaches. If the Pokémon lets its strange power erupt, apparently the pain subsides for a while.", price: "9999.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png')
+Item.create!(name: "Psyduck", species: "Water", description: "Psyduck is constantly beset by headaches. If the Pokémon lets its strange power erupt, apparently the pain subsides for a while.", price: "9999.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png')
 
-# Item.create(name: "Charmander", species: "Fire", description: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.", price: "18000", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png')
+Item.create(name: "Charmander", species: "Fire", description: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.", price: "18000", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png')
 
-# Item.create(name: "Squirtle", species: "Water", description: "When it retracts its long neck into its shell, it squirts out water with vigorous force.", price: "19200", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png')
+Item.create(name: "Squirtle", species: "Water", description: "When it retracts its long neck into its shell, it squirts out water with vigorous force.", price: "19200", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png')
 
-# Item.create(name: "Jigglypuff", species: "Fairy", description: "Jigglypuff has top-notch lung capacity, even by comparison to other Pokémon. It won’t stop singing its lullabies until its foes fall asleep.", price: "20100", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png')
-
-
-# Item.create(name: "Pikachu", species: "Electric", description: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.", price: "30000", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png')
-
-# Item.create(name: "Diglett", species: "Ground", description: "If a Diglett digs through a field, it leaves the soil perfectly tilled and ideal for planting crops.", price: "10100", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png')
-
-# Item.create(name: "Vulpix", species: "Fire", description: "While young, it has six gorgeous tails. When it grows, several new tails are sprouted.", price: "17800", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png')
-
-# Item.create(name: "Poliwhirl", species: "Water", description: "Staring at the swirl on its belly causes drowsiness. This trait of Poliwhirl’s has been used in place of lullabies to get children to go to sleep.", price: "10298.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/061.png')
+Item.create(name: "Jigglypuff", species: "Fairy", description: "Jigglypuff has top-notch lung capacity, even by comparison to other Pokémon. It won’t stop singing its lullabies until its foes fall asleep.", price: "20100", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png')
 
 
-# Item.create(name: "Ponyta", species: "Fire", description: "It can’t run properly when it’s newly born. As it races around with others of its kind, its legs grow stronger.", price: "10298.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/061.png')
+Item.create(name: "Pikachu", species: "Electric", description: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.", price: "30000", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png')
 
-# Item.create(name: "Slowpoke", species: "Water", description: "Slow-witted and oblivious, this Pokémon won’t feel any pain if its tail gets eaten. It won’t notice when its tail grows back, either.", price: "12398.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/078.png')
+Item.create(name: "Diglett", species: "Ground", description: "If a Diglett digs through a field, it leaves the soil perfectly tilled and ideal for planting crops.", price: "10100", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png')
+
+Item.create(name: "Vulpix", species: "Fire", description: "While young, it has six gorgeous tails. When it grows, several new tails are sprouted.", price: "17800", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png')
+
+Item.create(name: "Poliwhirl", species: "Water", description: "Staring at the swirl on its belly causes drowsiness. This trait of Poliwhirl’s has been used in place of lullabies to get children to go to sleep.", price: "10298.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/061.png')
+
+
+Item.create(name: "Ponyta", species: "Fire", description: "It can’t run properly when it’s newly born. As it races around with others of its kind, its legs grow stronger.", price: "10298.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/061.png')
+
+Item.create(name: "Slowpoke", species: "Water", description: "Slow-witted and oblivious, this Pokémon won’t feel any pain if its tail gets eaten. It won’t notice when its tail grows back, either.", price: "12398.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/078.png')
 
 
 
 # #     Grow-ups
 
-# Item.create(name: "Raichu", species: "Electric", description: "Its long tail serves as a ground to protect itself from its own high-voltage power.", price: "41213.8", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png')
+Item.create(name: "Raichu", species: "Electric", description: "Its long tail serves as a ground to protect itself from its own high-voltage power.", price: "41213.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png')
 
-# Item.create(name: "Wigglytuff", species: "Electric", description: "The more air it takes in, the more it inflates. If opponents catch it in a bad mood, it will inflate itself to an enormous size to intimidate them.", price: "21213.8", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png')
+Item.create(name: "Wigglytuff", species: "Electric", description: "The more air it takes in, the more it inflates. If opponents catch it in a bad mood, it will inflate itself to an enormous size to intimidate them.", price: "21213.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/040.png')
 
-# Item.create(name: "Sandslash", species: "Ground", description: "The drier the area Sandslash lives in, the harder and smoother the Pokémon’s spikes will feel when touched.", price: "20187.8", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png')
+Item.create(name: "Sandslash", species: "Ground", description: "The drier the area Sandslash lives in, the harder and smoother the Pokémon’s spikes will feel when touched.", price: "20187.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png')
 
-# Item.create(name: "Blastoise", species: "Water", description: "It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell", price: "12181.8", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png')
+Item.create(name: "Blastoise", species: "Water", description: "It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell", price: "12181.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png')
 
-# Item.create(name: "Wartortle", species: "Water", description: "It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.", price: "12398.8", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png')
+Item.create(name: "Wartortle", species: "Water", description: "It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.", price: "12398.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png')
 
-# Item.create(name: "Feraligatr", species: "Water", description: "Feraligatr intimidates its foes by opening its huge mouth. In battle, it will kick the ground hard with its thick and powerful hind legs to charge at the foe at an incredible speed.", price: "12398.8", collection_id: "2", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/160.png')
+Item.create(name: "Feraligatr", species: "Water", description: "Feraligatr intimidates its foes by opening its huge mouth. In battle, it will kick the ground hard with its thick and powerful hind legs to charge at the foe at an incredible speed.", price: "12398.8", collection_id: "1", img_url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/160.png')
 
 
 p "seed successful"
