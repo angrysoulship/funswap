50257f310c75afb91f3222e4f995e14196b42e8f
eo hao <hash1hao@gmail.com>
2021-07-21 15:17:18 +0800

    sign up, log in, pass reset done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Item.destroy_all
 Collection.destroy_all
 
 Collection.create(name: "Young Pokemons!", user_id: 1)
