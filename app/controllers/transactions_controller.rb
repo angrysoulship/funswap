4e688eda01e3b8618aa1e490fe84cbe90bc6555d
eo hao <hash1hao@gmail.com>
2021-07-22 18:59:08 +0800

    Sign up form bug fixed, Transaction Controller finailizing...waiting for balance table

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,20 +4,36 @@ class TransactionsController < ApplicationController
 
   def new
     @item = Item.find(params[:item_id])
+    @collection_id = @item.collection_id
     @transaction = Transaction.new
     authorize @transaction
+
   end
 
   def create
-    @transaction = Transaction.new(transaction_params)
     @item = Item.find(params[:item_id])
-    @transaction.item = @item
-    @transaction.seller_id = @transaction.item.collection.user.id
+    @buyer_id = current_user.id
+
+    @transaction = Transaction.new( item_id: @item.id, buyer_id: @buyer_id)
+
+    # @transaction.item = @item
+    # @seller_collection = Collection.find(@transaction.item.collection_id)
+
+    #change item collection from seller_collection, to buyer_collection
+
+    @item.collection_id = current_user.collections[0].id
+
+    raise
+
+    # @seller = User.find(@seller_collection.user_id)
+    # @buyer = User.find(@buyer_id)
+
+    @transaction.seller_id = @transaction.item.collection_id.user.id
     @transaction.buyer_id = current_user.id
-    seller = User.find(@transaction.seller_id)
-    seller.balance += @transaction.item.price
-    buyer = User.find(@transaction.buyer_id)
-    buyer.balance -= @transaction.item.price
+    # seller = User.find(@transaction.seller_id)
+    # seller.balance += @transaction.item.price
+    # buyer = User.find(@transaction.buyer_id)
+    # buyer.balance -= @transaction.item.price
 
     authorize @transaction
     if @transaction.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_many :collections, dependent: :destroy
-      
+
   # validates :name, presence: true, uniqueness: true
-  
+
 end
