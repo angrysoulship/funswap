3bd800521b8982389d48cae3960899d275f3df0c
ivyf <iveee1023@gmail.com>
2021-07-23 15:10:43 +0800

    nah

--- db/migrate/20210722070901_create_balances.rb
+++ db/migrate/20210722070901_create_balances.rb
@@ -1,7 +1,7 @@
 class CreateBalances < ActiveRecord::Migration[6.0]
   def change
     create_table :balances do |t|
-      t.float :amount, default: 10000.00
+      t.float :amount, default:10000.00
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
